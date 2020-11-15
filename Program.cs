db999b472de1a62248f70e69b2ade507fb2548d7
kamal7241 <kamal.korney@gmail.com>
2020-11-15 07:34:16 +0200

    First Commit

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("this is HelloWorld Project");
+            Console.WriteLine("First Commit");
             Console.ReadKey();
         }
     }
